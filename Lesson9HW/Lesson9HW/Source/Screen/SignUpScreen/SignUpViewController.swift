26fb182f2ba0fadf80ede18e1c94683b83ea3b87
RuslanWorkGit <lulka758@gmail.com>
2024-12-23 23:08:14 +0200

    add scrollView in storyboard
    add scrollView into ContentView, add View into ScrollView and add stackView into View

--- Lesson9HW/Lesson9HW/Source/Screen/SignUpScreen/SignUpViewController.swift
+++ Lesson9HW/Lesson9HW/Source/Screen/SignUpScreen/SignUpViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 class SignUpViewController: UIViewController {
     
     @IBOutlet weak var contentView: SignUpView!
-    
+    @IBOutlet weak var scrollView: UIScrollView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         subscrubeNotifications()
